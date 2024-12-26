8ac277d863ffc3da7b5754548265dc88f56a1d72
Chris Wiegand <chris@wiegandtech.net>
2024-12-25 23:37:11 -0700

    adjust stream tests to keep stream open

--- tests/SimpleTests.cs
+++ tests/SimpleTests.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using JetNet;
 
 namespace JetTests;
@@ -27,14 +28,29 @@ public class SimpleTests
     }
 
     [TestMethod]
-    public void TestHelloWorldStream()
+    public void TestHelloWorldUtf8Stream()
     {
         MemoryStream ms = new MemoryStream();
-        using (StreamWriter sw = new StreamWriter(ms))
-        {
+        using (StreamWriter sw = new StreamWriter(ms, Encoding.UTF8, -1, true))
             sw.Write(HelloWorldJson);
-            ms.Position = 0;
+        ms.Position = 0;
 
+        JsonParser p = new JsonParser(ms);
+        JsonParseResult result = p.ProcessJson();
+
+        Test_ValidateHelloWorld(result);
+    }
+
+    [TestMethod]
+    public void TestHelloWorldUtf8StreamReader()
+    {
+        MemoryStream ms = new MemoryStream();
+        using (StreamWriter sw = new StreamWriter(ms, Encoding.UTF8, -1, true))
+            sw.Write(HelloWorldJson);
+        ms.Position = 0;
+
+        using (StreamReader sr = new StreamReader(ms))
+        {
             JsonParser p = new JsonParser(ms);
             JsonParseResult result = p.ProcessJson();
 
@@ -43,21 +59,33 @@ public class SimpleTests
     }
 
     [TestMethod]
-    public void TestHelloWorldStreamReader()
+    public void TestHelloWorldUnicodeStream()
     {
         MemoryStream ms = new MemoryStream();
-        using (StreamWriter sw = new StreamWriter(ms))
-        {
+        using (StreamWriter sw = new StreamWriter(ms, Encoding.Unicode, -1, true))
             sw.Write(HelloWorldJson);
-            ms.Position = 0;
+        ms.Position = 0;
 
-            using (StreamReader sr = new StreamReader(ms))
-            {
-                JsonParser p = new JsonParser(ms);
-                JsonParseResult result = p.ProcessJson();
+        JsonParser p = new JsonParser(ms);
+        JsonParseResult result = p.ProcessJson();
 
-                Test_ValidateHelloWorld(result);
-            }
+        Test_ValidateHelloWorld(result);
+    }
+
+    [TestMethod]
+    public void TestHelloWorldUnicodeStreamReader()
+    {
+        MemoryStream ms = new MemoryStream();
+        using (StreamWriter sw = new StreamWriter(ms, Encoding.Unicode, -1, true))
+            sw.Write(HelloWorldJson);
+        ms.Position = 0;
+
+        using (StreamReader sr = new StreamReader(ms))
+        {
+            JsonParser p = new JsonParser(ms);
+            JsonParseResult result = p.ProcessJson();
+
+            Test_ValidateHelloWorld(result);
         }
     }
 
